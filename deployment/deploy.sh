88e07c57b13d9823aee20689ebf8679d43ee260d
ciprianPes <75477171+ciprianPes@users.noreply.github.com>
2021-10-16 20:09:34 +0300

    Deploy Elastic and kibana
    Deploy Elastic and kibana yaml

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,8 @@
 microk8s kubectl apply -f zoo.yaml
 microk8s kubectl apply -f kafka.yaml
 microk8s kubectl apply -f RSSparser.yaml
+
+microk8s kubectl apply -f Volumes.yaml
+
+microk8s kubectl apply -f ElasticSearch.yaml
+microk8s kubectl apply -f kibana.yaml
